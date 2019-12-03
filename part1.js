cf7241dd76c1c2751d34f9e5c15ed74b43a7542e
jyotsna-girhotra <jyotsnamehta@gmail.com>
2019-12-03 15:04:30 -0500

    Update part1.js

--- part1.js
+++ part1.js
@@ -1,1 +1,13 @@
+const fs = require('fs');
 
+let input = fs.readFileSync("input.txt", "utf-8");
+mass = input.split("\n");
+console.log('after calling readFile');
+
+let total = 0;
+
+for(var i=0; i<mass.length; i++) {
+    fuelTotal += Math.floor(mass[i]/3) - 2;
+}
+console.log ("mass length =" , mass.length);
+console.log("Total Fuel = ", fuelTotal);
